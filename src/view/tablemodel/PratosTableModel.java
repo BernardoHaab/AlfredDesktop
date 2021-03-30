eaf9a7db194ef7c72ae1717da9b230b3f28742bf
BernardoHaab <bernardohaab@gmail.com>
2021-03-30 18:15:38 -0300

    First commit

---
+++ src/view/tablemodel/PratosTableModel.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view.tablemodel;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author be_ha
+ */
+public class PratosTableModel extends AbstractTableModel{
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getValueAt(int i, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public String getColumnName(int column) {
+       switch (column){
+            case 0 : return "Nome";
+            case 1 : return "Descrição";
+            case 2 : return "Preço";
+            default : return "";
+        }
+    }
+}
